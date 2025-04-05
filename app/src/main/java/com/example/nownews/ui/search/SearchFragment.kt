c6b4efe4b839c6d6f7e178009539c8e803e64c62
khushi <khushi04@edu.sait.ca>
2025-04-05 09:18:06 -0600

    added search bar in search news section

--- app/src/main/java/com/example/nownews/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/nownews/ui/search/SearchFragment.kt
@@ -1,20 +1,37 @@
 package com.example.nownews.ui.search
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.nownews.R
+import com.example.nownews.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
+
+    private lateinit var binding: FragmentSearchBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        binding = FragmentSearchBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.searchButton.setOnClickListener {
+            val searchQuery = binding.searchEditText.text.toString()
+            binding.searchText.text = "Search news for title: $searchQuery"
+        }
+
     }
 
 }
