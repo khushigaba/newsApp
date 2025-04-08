e673650bb01f2517f2b4f11eeaf793ae1d2be241
khushi <khushi04@edu.sait.ca>
2025-04-08 02:09:43 -0600

    modifying  news api to integrate news in search news screen

--- app/src/main/java/com/example/nownews/api/NewsApi.kt
+++ app/src/main/java/com/example/nownews/api/NewsApi.kt
@@ -97,5 +97,47 @@ object NewsApi {
         }
     }
 
+    fun searchNewsByTitle(query: String): List<NewsArticle>? {
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url("https://newsapi.org/v2/everything?q=$query&apiKey=$API_KEY&page=1&pageSize=15")
+            .build()
+
+        return try {
+            val response = client.newCall(request).execute()
+            val responseBody = response.body?.string() ?: ""
+
+            val jsonObject = JSONObject(responseBody)
+            val articlesArray = jsonObject.getJSONArray("articles")
+
+            val newsList = mutableListOf<NewsArticle>()
+
+            for (i in 0 until articlesArray.length()) {
+                val articleJson = articlesArray.getJSONObject(i)
+
+                val title = articleJson.getString("title")
+                val description = articleJson.optString("description", null)
+                val url = articleJson.getString("url")
+                val imageUrl = articleJson.optString("urlToImage", null)
+                val publishedAt = articleJson.getString("publishedAt")
+                val sourceName = articleJson.getJSONObject("source").optString("name", "")
+                val newsArticle = NewsArticle(
+                    title = title,
+                    description = description,
+                    url = url,
+                    imageUrl = imageUrl,
+                    publishedAt = publishedAt,
+                    source = sourceName
+                )
+
+                newsList.add(newsArticle)
+            }
+
+            newsList
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
 
 }
